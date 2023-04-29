4743af0e902241f02752b37c912c9ec3089c94b1
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-04-29 09:03:43 -0400

    Added HTML for all time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+let currentDay = document.getElementById("currentDay");
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
@@ -20,4 +21,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  currentDay = dayjs();
+  $('#currentDay').text(currentDay.format('MMM DD, YYYY'));
 });
