8258e4ad46e204ad1165d108d60b090fb71118c0
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-04-29 16:51:05 -0400

    colours change when time changes

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+//_________Global Variables______________________________
+const saveButton = document.querySelectorAll(".saveBtn");
 let currentDay = document.getElementById("currentDay");
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -15,7 +15,28 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+  //________Change color based on time function______________________
+  let current = dayjs()
+  let hourNow = current.hour();
+  $('.time-block').each(function() {
+    //parse the data into an integer
+    //Split the id into two parts where index 1 is the integer needed.
+    const timeElement = parseInt($(this).attr("id").split("-")[1]);
+    if (hourNow > timeElement) {
+      $(this).addClass("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+    } else if (hourNow == timeElement) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).addClass("future");
+      $(this).removeClass("present");
+    }
+  });
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
